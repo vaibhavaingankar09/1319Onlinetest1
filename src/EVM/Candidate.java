24851b6b9f368f7d59364c6627a3737299a54899
Vaibhav Vaingankar <vbhvvaingankar9@gmail.com>
2015-07-09 15:55:34 +0530

    version 1.2

---
+++ src/EVM/Candidate.java
@@ -0,0 +1,20 @@
+package EVM;
+
+public class Candidate {
+	
+	int candidateNum;
+	String candidateName;
+	
+	public int getCandidateNum() {
+		return candidateNum;
+	}
+	public void setCandidateNum(int candidateNum) {
+		this.candidateNum = candidateNum;
+	}
+	public String getCandidateName() {
+		return candidateName;
+	}
+	public void setCandidateName(String candidateName) {
+		this.candidateName = candidateName;
+	}
+}
---
+++ src/EVM/Constituency.java
@@ -0,0 +1,20 @@
+package EVM;
+
+public class Constituency {
+	
+	int constNumber;
+	String constName;
+	
+	public int getConstNumber() {
+		return constNumber;
+	}
+	public void setConstNumber(int constNumber) {
+		this.constNumber = constNumber;
+	}
+	public String getConstName() {
+		return constName;
+	}
+	public void setConstName(String constName) {
+		this.constName = constName;
+	}
+}
